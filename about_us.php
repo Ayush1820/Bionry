eec1fea569ac4d6a8b422a8248cbc37276f123ea
Ayush Thakur <ayushthakur189202@gmail.com>
2023-04-02 14:49:32 +0530

    Removed unnessary files

--- about_us.php
+++ about_us.php
@@ -3,21 +3,28 @@
 <body class="hold-transition skin-blue layout-top-nav">
 	<?php include 'includes/navbar.php'; ?>
 
-    <div class="about_div">
-        <h1>Bionry Deveopers</h1>
-        <div>
-            <h4>Ayush Thakur</h4>
+    <div id="aboutUsNew" style="background-color: #659DBD; color: black; height: 630px; display:flex; justify-content: center;">
+        <div style="display: flex;">
+            <div style="margin-left: -150px;">
+                <h1>Bionry Developers</h1>
+            </div>
+            <div style="margin-top:120px; margin-left: 100px;">
+                <h1>Ayush Thakur</h1>
+            </div>
+            <div style="margin-top:120px; margin-left: 50px;">
+                <h1>Ankit Pande</h1>
+            </div>
+            <div style="margin-top:120px; margin-left: 50px;">
+                <h1>Sadik Khan</h1>
+            </div>
         </div>
-        <div>
-            <h4>Ankit Pande</h4>
-        </div>
-        <div>
-            <h4>Sadik Khan</h4>
-        </div>
-    </div>
+</div>
   
   	<?php include 'includes/footer.php'; ?>
 
 <?php include 'includes/scripts.php'; ?>
 </body>
 </html>
+
+
+
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <header class="main-header">
-  <nav class="navbar navbar-static-top">
+  <nav class="navbar navbar-static-top"  style="background-color:#1A1A1D; color:black;">
     <div class="container">
       <div class="navbar-header">
         <a href="index.php" class="navbar-brand"><b>Bionry</b></a>
