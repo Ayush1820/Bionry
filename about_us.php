27c66d669ee6d5eed050993e1209ab74b07994b5
Ayush Thakur <ayushthakur189202@gmail.com>
2023-04-02 13:41:21 +0530

    initial commit in Bionry

---
+++ about_us.php
@@ -0,0 +1,23 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+	<?php include 'includes/navbar.php'; ?>
+
+    <div class="about_div">
+        <h1>Bionry Deveopers</h1>
+        <div>
+            <h4>Ayush Thakur</h4>
+        </div>
+        <div>
+            <h4>Ankit Pande</h4>
+        </div>
+        <div>
+            <h4>Sadik Khan</h4>
+        </div>
+    </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 Author <a href="https://freeprojectscodes.com/">Bionry Developers</a></strong>
+    </div>
+</footer>
